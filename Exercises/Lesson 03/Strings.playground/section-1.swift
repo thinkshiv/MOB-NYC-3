986a17243d948edf486bcd52c5b382df015e4f93
thinkshiv <shiven.ramji@gmail.com>
2015-04-30 17:30:49 -0400

    Added Lesson 1 assignment and in class playground assignments

---
+++ Exercises/Lesson 03/Strings.playground/section-1.swift
@@ -0,0 +1,18 @@
+// Strings
+
+"hello"
+
+// Using println lets us print to the Console in a running iPhone app.
+println("Hello, World!")
+
+// Concatenation, same syntax as numeric addition, but
+"Hello, " + "World!"
+"Hello, World!"
+
+"Hello, Toshi!"
+"Whatever"
+
+println("Hey Farewell")
+println(1 + 2)
+
+
