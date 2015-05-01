e7539d0659aa222e70dcca97a9d827f720fd8c15
thinkshiv <shiven.ramji@gmail.com>
2015-05-01 00:54:06 -0400

    Adding Lesson 4 exercises

---
+++ Exercises/Lesson 04/Variables.playground/Contents.swift
@@ -0,0 +1,31 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import Foundation
+
+var str = "Hello, playground"
+
+/*
+let feetPerMeter = 3.28
+let footballFieldLength = 300.0
+
+let metersinFootballField = footballFieldLength / feetPerMeter
+*/
+
+// Loops
+
+var x = 0
+
+
+let isItPrime = 289
+
+for (var i = 2; i < isItPrime; i++) {
+    //if isItPrime % i == 0 {
+      //  println("composite")
+        //break
+    //}
+    
+    
+}
+
+
